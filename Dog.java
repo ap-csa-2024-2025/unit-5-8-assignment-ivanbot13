c7b9c448ef2cd906b0e75894c3e66b53a4062354
Ivan <ikduong100@student.hbuhsd.edu>
2025-01-08 19:04:16 +0000

    save

--- Dog.java
+++ Dog.java
@@ -5,12 +5,41 @@ public class Dog
   private static int numDogs = 0;
 
   // Non-default Dog constructor
+  public Dog(String name){
+    this.name = name;
+    numDogs++;
+    this.ID = numDogs * numDogs; //example
+  }
 
   // Default Dog constructor
+  public Dog(String name){
+    this.name = name;
+  }
 
   // Setters - instance variables only
+  public void setName(String iName){
+    name = iName;
+  }
 
+  public void setID(int iID){
+    ID = iID;
+  }
+
+  public void setNum(int iNum){
+    numDogs = iNum;
+  }
+  
   // Getters - instance variables only
+  public String getName(){
+    return name;
+  }
+  
+  public int getID(){
+    return ID;
+  }
 
+  public int getNum(){
+    return numDogs;
+  }
   // method getNumDogs
 }
